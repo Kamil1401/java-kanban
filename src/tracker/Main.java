0a8781feec43d295787305749243b3adef6af3b8
Kamil Medjidov <captain-kam@yandex.ru>
2025-08-08 22:07:19 +0400

    Добавил тесты для класса FailBackedTaskManager

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -1,29 +1,23 @@
 package tracker;
 
 import tracker.managers.Managers;
-import tracker.managers.taskmanager.FileBackedTaskManager;
 import tracker.managers.taskmanager.TaskManager;
 import tracker.model.Epic;
-import tracker.model.Subtask;
 import tracker.model.Task;
 
 public class Main {
     public static void main(String[] args) {
-        TaskManager taskManager = Managers.getDefault();
-        Task dinner = new Task("Ужин", "Приготовить ужин");
-        taskManager.addTask(dinner);
-        System.out.println(dinner.getId());
-        Epic repair = new Epic("Ремонт", "Начать ремонт в квартире");
-        taskManager.addEpic(repair);
-        Subtask hiring = new Subtask("Нанять бригаду", "Позвонить мастерам", repair.getId());
-        Subtask materials = new Subtask("Съездить в Добрострой", "Купить материалы", repair.getId());
-        taskManager.addSubtask(hiring);
-        taskManager.addSubtask(materials);
-        Epic sale = new Epic("Продажа", "Продать машины");
-        taskManager.addEpic(sale);
-        Subtask advertisement = new Subtask("Реклама", "Разместить на авито", sale.getId());
-        Subtask service = new Subtask("Сервис", "Отвезти машину в сервис", sale.getId());
-        taskManager.addSubtask(advertisement);
-        taskManager.addSubtask(service);
+        TaskManager manager = Managers.getDefault();
+
+        Task task1 = new Task("Name 1", "Task");
+        manager.addTask(task1);
+        Task task2 = new Task("Name 2", "Task");
+        manager.addTask(task2);
+        Epic epic1 = new Epic("Name 1", "Epic");
+        manager.addEpic(epic1);
+        System.out.println(task1.getId() + ", " + task2.getId() + ", " + epic1.getId());
+
+
+
     }
 }
